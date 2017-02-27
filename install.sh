50dbecf98efd032bc0e4bfbc8c7a76f01c6fa938
Ian Park <asbear@gmail.com>
2017-02-27 23:13:15 +0000

    Remove chartJS

--- install.sh
+++ install.sh
@@ -3,3 +3,5 @@ curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 npm install
+
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 7540
